f3dec34217f5213ed35860bf9383ee2db3e16af3
itnhtu <itnhtu@gmail.com>
2023-03-22 01:30:50 +0700

    done FE

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../img/logo.png'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Copyright2023 ♥ <b>HT</b></span>
+    </footer>
+  )
+}
+
+export default Footer
